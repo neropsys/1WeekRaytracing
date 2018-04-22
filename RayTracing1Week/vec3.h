c8448cc28a2e937cb2619b0afd08a22bb6154ad0
neropsys <neropsys@gmail.com>
2018-04-23 02:36:59 +0900

    chapter 5
    added camera, sphre, hitable interface

--- RayTracing1Week/vec3.h
+++ RayTracing1Week/vec3.h
@@ -92,7 +92,7 @@ class vec3 {
 		}
 		friend inline float dot(const vec3& v1, const vec3& v2)
 		{
-			return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v1.e[2];
+			return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2];
 		}
 
 		friend inline vec3 cross(const vec3& v1, const vec3& v2)
