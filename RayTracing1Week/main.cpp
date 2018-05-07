75f0f14213976f5047fd073fc10be5126335fca6
neropsys <neropsys@gmail.com>
2018-05-07 22:44:04 +0900

    final result

--- RayTracing1Week/main.cpp
+++ RayTracing1Week/main.cpp
@@ -10,6 +10,7 @@
 #include <time.h>
 #include <ppl.h>
 #include <vector>
+#define DRAND48 static_cast <float> (rand()) / static_cast <float> (RAND_MAX)
 vec3 color(const ray& r, hitable* world, int depth)
 {
 	hit_record rec;
@@ -30,8 +31,46 @@ vec3 color(const ray& r, hitable* world, int depth)
 		float t = 0.5 * (unit_direction.y() + 1.0);
 		return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(.5, .7, 1.0);
 	}
-}
+ }
+std::vector<hitable*> random_scene()
+{
+	int n = 500;
+	std::vector<hitable*> list;
+	list.reserve(n + 1);
 
+	list.emplace_back(new sphere(vec3(0, -1000, 0), 1000, new lambertian(vec3(0.5, 0.5, 0.5))));
+	int i = 1;
+	for(int a = -11; a < 11; a++)
+	{
+		for(int b = -11; b < 11; b++)
+		{
+			float chose_mat = DRAND48;
+			vec3 center(a + 0.9 * DRAND48, 0.2, b + 0.9 * DRAND48);
+			if((center - vec3(4, 0.2, 0)).length() > 0.9)
+			{
+				if(chose_mat < 0.8)
+				{
+					list.emplace_back(new sphere(center, 0.2, new lambertian(vec3(DRAND48 *DRAND48, DRAND48*DRAND48, DRAND48*DRAND48))));
+				}
+				else if(chose_mat < 0.95)
+				{
+					list.emplace_back(new sphere(center, 0.2, new metal(vec3(0.5 * (1 + DRAND48), 0.5 * (1 + DRAND48), 0.5*(1 + DRAND48)), 0.5*DRAND48)));
+				}
+				else
+				{
+					list.emplace_back(new sphere(center, 0.2, new dielectric(1.5)));
+				}
+			}
+		}
+
+		
+	}
+
+	list.emplace_back(new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5)));
+	list.emplace_back(new sphere(vec3(-4, 1, 0), 1, new lambertian(vec3(0.4, 0.2, 0.1))));
+	list.emplace_back(new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.7, 0.6, 0.5), 0)));
+	return list;
+}
 int main()
 {
 	struct RGB {
@@ -44,8 +83,8 @@ int main()
 	fstream file;
 	file.open("result.ppm", fstream::out);
 
-	int nx = 800;
-	int ny = 400;
+	int nx = 3200;
+	int ny = 1600;
 	int ns = 100;
 	file << "P3\n" << nx << " " << ny << "\n255\n";
 	
@@ -53,21 +92,21 @@ int main()
 	vec3 horz(4, 0, 0);
 	vec3 vert(0, 2, 0);
 	vec3 origin(0, 0, 0);
-	std::vector<hitable*> list;
+	std::vector<hitable*> list = random_scene();
 
-	list.emplace_back(new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5))));
-	list.emplace_back(new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0))));
-	list.emplace_back(new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.0)));
-	list.emplace_back(new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5)));
-	list.emplace_back(new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5)));
+// 	list.emplace_back(new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5))));
+// 	list.emplace_back(new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0))));
+// 	list.emplace_back(new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 0.0)));
+// 	list.emplace_back(new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5)));
+// 	list.emplace_back(new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5)));
 	
 
 	hitable* world = new hitable_list(list.data(), list.size());
 
-	vec3 lookfrom(3, 3, 2);
-	vec3 lookat(0, 0, -1);
+	vec3 lookfrom(10, 1, 7);
+	vec3 lookat(0, 0, 0);
 	float dist_to_focus = (lookfrom - lookat).length();
-	float aperture = 2.0;
+	float aperture = 0.0;
 
 
 	Camera cam(lookfrom,lookat, vec3(0, 1, 0), 20, float(nx) / float(ny), aperture, dist_to_focus);
